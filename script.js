d123d2d908f03dfd27f661a076e179ddd0ba02bc
Lisa Lee <llee28@cnm.edu>
2019-11-12 05:54:18 -0700

    add updated files

--- script.js
+++ script.js
@@ -3,10 +3,16 @@ window.onload = function () {
 };
 
 function initLiteraryMutilator() {
-	let form = document.getElementById("red");
-
-	form.addEventListener("mouseover", (event) => {
-		handleItemForm(event, form);
-	});
+	let paragraph1 = document.getElementById("paragraph1");
+	paragraph1.addEventListener("mouseover", (event) => {
+		handleParagraph1(event, paragraph1);
+	})
 }
+// Colors paragraph red
+function handleParagraph1(event, paragraphRef) {
+	if(event.preventDefault) {
+		event.preventDefault();
+	}
+	let
 
+}
