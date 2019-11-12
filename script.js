71f009a4ea9a6961e402e5bcc5cf6a165a189f8f
Lisa Lee <llee28@cnm.edu>
2019-11-12 04:35:24 -0700

    add event listener to change color red

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
 window.onload = function () {
-	initColorPicker();
+	initLiteraryMutilator();
 };
 
-function initColorPicker() {
-	let colorBox = document.getElementById("color-box");
-	let rgb = {
-		red: document.getElementById("red"),
-		green: document.getElementById("green"),
-		blue: document.getElementById("blue")
-	};
-	let colorPickers = document.getElementsByClassName("picker");
-	setColorPickerEventListeners(colorBox, rgb, colorPickers);
+function initLiteraryMutilator() {
+	let form = document.getElementById("red");
+
+	form.addEventListener("mouseover", (event) => {
+		handleItemForm(event, form);
+	});
 }
 
-
-}
